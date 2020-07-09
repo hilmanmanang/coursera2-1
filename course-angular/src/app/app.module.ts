d7d9377ae17ef6d8f0ace3d7a64265eceb078ffd
hilmanmanang@gmail.com <hilmanmanang@gmail.com>
2020-07-09 11:18:24 +0800

    ii

--- course-angular/src/app/app.module.ts
+++ course-angular/src/app/app.module.ts
@@ -4,6 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -12,7 +18,10 @@ import { MenuComponent } from './menu/menu.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
