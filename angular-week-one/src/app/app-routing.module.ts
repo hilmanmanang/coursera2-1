3afd9d1f3154c466fbd46ff157ce59b4d9acf9f3
hilmanmanang@gmail.com <hilmanmanang@gmail.com>
2020-08-24 14:29:05 +0800

    save

--- angular-week-one/src/app/app-routing.module.ts
+++ angular-week-one/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { routes } from './app-routing/routes';
 
-
-const routes: Routes = [];
+// const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
