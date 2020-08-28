607dc2c527cd924a24e1d70eb1d012927a314745
hilmanmanang@gmail.com <hilmanmanang@gmail.com>
2020-08-28 16:13:35 +0800

    promise part 2

--- angular-week-one/src/app/app.module.ts
+++ angular-week-one/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
@@ -64,6 +65,7 @@ import { LoginComponent } from './login/login.component';
     MatCheckboxModule,
     MatSelectModule,
     MatSlideToggleModule,
+    MatProgressSpinnerModule
   ],
   providers: [
     DishService,
