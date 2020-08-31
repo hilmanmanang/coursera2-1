1a02771a1fd0ee21e10786b40dd15c07f9254929
hilmanmanang@gmail.com <hilmanmanang@gmail.com>
2020-08-31 08:20:01 +0800

    save

--- angular-week-one/src/app/home/home.component.ts
+++ angular-week-one/src/app/home/home.component.ts
@@ -22,7 +22,9 @@ export class HomeComponent implements OnInit {
   dish: Dish;
   dishErrMess: string;
   promotion: Promotion;
+  promotionErrMess: string;
   leader: Leader;
+  leaderErrMess: string;
 
   constructor(
     private dishService: DishService,
@@ -33,8 +35,8 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.dishService.getFeaturedDish().subscribe(dish => this.dish = dish, errmess => this.dishErrMess = <any>errmess);
-    this.promotionService.getFeaturedPromotion().subscribe(promotion => this.promotion = promotion);
-    this.leaderService.getFeaturedLeader().subscribe(leader => this.leader = leader);
+    this.promotionService.getFeaturedPromotion().subscribe(promotion => this.promotion = promotion, errmess => this.promotionErrMess = <any>errmess);
+    this.leaderService.getFeaturedLeader().subscribe(leader => this.leader = leader, errmess => this.leaderErrMess = <any>errmess);
   }
 
 }
--- angular-week-one/src/app/services/leader.service.ts
+++ angular-week-one/src/app/services/leader.service.ts
@@ -2,24 +2,44 @@ import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
 import { Observable, of } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { delay } from 'rxjs/operators';import { Promotion } from '../shared/promotion';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { baseURL } from '../shared/baseurl';
+import { map, catchError } from 'rxjs/operators';
+import { ProcessHTTPMsgService } from './process-httpmsg.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LeaderService {
 
-  constructor() { }
+  constructor(    
+    private http: HttpClient,
+    private processHTTPMsgService: ProcessHTTPMsgService
+  ) { }
 
   getLeaders(): Observable<Leader[]> {
-    return of(LEADERS).pipe(delay(2000));
+    return this.http.get<Leader[]>(baseURL + 'leadership').pipe(catchError(this.processHTTPMsgService.handleError));
   }
 
   getLeader(id: string): Observable<Leader> {
-    return of(LEADERS.filter((leader) => (leader.id === id))[0]).pipe(delay(2000));
+    return this.http.get<Leader>(baseURL + 'leadership/' + id).pipe(catchError(this.processHTTPMsgService.handleError));
   }
 
   getFeaturedLeader(): Observable<Leader> {
-    return of(LEADERS.filter((leader) => leader.featured)[0]).pipe(delay(2000));
+    return this.http.get<Leader[]>(baseURL + 'leadership?featured=true').pipe(map(leadership => leadership[0])).pipe(catchError(this.processHTTPMsgService.handleError));
+  }
+
+  getLeaderIds(): Observable<number[] | any> {
+    return this.getLeaders().pipe(map(leadership => leadership.map(leader => leader.id))).pipe(catchError(error => error));
+  }
+
+  putLeader(leader: Leader): Observable<Leader> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    return this.http.put<Leader>(baseURL + 'leadership/' + leader.id, leader, httpOptions).pipe(catchError(this.processHTTPMsgService.handleError))
   }
 }
